651e68a347384f82b7bab0548259968f2adbef64
Chris <clj@ollure.com>
2020-08-03 17:21:31 -0500

    basic game built,

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => { 
 const dino = document.querySelector('.dino')
+const grid = document.querySelector('.grid')
+const alert = document.getElementById('alert')
+
 let isJumping = false 
 let gravity = 0.9
+let isGameOver = false
 
 function control(e){
  	if (e.keyCode === 32){
@@ -47,6 +51,33 @@ function jump() {
 		console.log(dino.style.bottom )
 	},20)
 } 
+
+function generateObstacles() {
+	let randomTime = Math.random() * 4000 
+	let obstaclePosition = 1000
+	const obstacle =  document.createElement('div')
+	if (!isGameOver)obstacle.classList.add('obstacle')
+	grid.appendChild(obstacle)
+	obstacle.style.left = obstaclePosition + 'px'
+
+	let timerId = setInterval(function() {
+		if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
+			alert.innerHTML = 'Game Over!'
+			isGameOver = true
+			//remove child divs \
+			//while (grid.firstChild){
+			//	grid.removeChild(grid.lastChild)
+			//}
+		}
+
+
+		obstaclePosition -=10
+		obstacle.style.left = obstaclePosition + 'px'
+	}, 20)
+	if (!isGameOver) setTimeout(generateObstacles, randomTime)
+
+}
+	generateObstacles()
 
 
 })
