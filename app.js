9e0f287d805b96d4c9a33a2b1d9cc16b5f467ee2
Chris <clj@ollure.com>
2020-08-06 22:17:37 -0500

    added gradient backgound attemp to add stars

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function control(e){
 	}
 document.addEventListener('keyup', control)
 
-position = 0
+let position = 0
 function jump() {
 	let count   = 0
 	let timerId = setInterval(function () {
@@ -81,3 +81,49 @@ function generateObstacles() {
 
 
 })
+
+
+// Populate the star field
+const numOfStars = 100;
+
+//
+for (let i = 0; i < numOfStars; i++) {
+  let star = document.createElement("div");
+    star.className = "star";
+    var xy = getRandomPosition();
+      star.style.top = xy[0] +'px';
+      star.style.left = xy[1] + 'px';
+    document.body.append(star);
+}
+
+const numOfStarsFade = 100;
+
+//
+for (let i = 0; i < numOfStarsFade; i++) {
+  let star = document.createElement("div");
+    star.className = "star2";
+    var xy = getRandomPosition();
+      star.style.top = xy[0] +'px';
+      star.style.left = xy[1] + 'px';
+    document.body.append(star);
+}
+
+
+
+function getRandomPosition() {
+  var y = window.innerWidth;
+  var x = window.innerHeight;
+    var randomX = Math.floor(Math.random()*x);
+    var randomY = Math.floor(Math.random()*y);
+    return [randomX,randomY];
+}
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 5000);
+}
+
+function showPage() {
+  document.getElementById("bouncing-loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
